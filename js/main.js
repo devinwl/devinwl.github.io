6e124760ed3c05d9e4f470687f648a07366a25fb
Devin Lumley <keiroshin@gmail.com>
2014-03-23 14:51:19 -0700

    Updates

--- js/main.js
+++ js/main.js
@@ -12,6 +12,12 @@
             kkeys.push(e.keyCode);
             if(kkeys.toString().indexOf(seq) >= 0) {
                 audio.play();
+                $('.alert').css('opacity', '1.0');
+                $('.alert').css('font-size', '36px');
+                $(audio).bind('ended', function() {
+                    $('.alert').css('opacity', '0.0');
+                    $('.alert').css('font-size', '0px');
+                });
                 kkeys = [];
             }
         })
