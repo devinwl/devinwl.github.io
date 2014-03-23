717cccfae307737ad7f04d04a2c3a3326ffd9ab4
Devin Lumley <keiroshin@gmail.com>
2014-03-23 14:01:22 -0700

    Site update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 (function($) {
-	$(document).ready(function() {
+    $(document).ready(function() {
 
-	});
+        var kkeys = [];
+        var seq = '38,38,40,40,37,39,37,39,66,65';
+
+        var audio = document.createElement('audio');
+        audio.setAttribute('src', '../resources/sounds/secret.mp3');
+        //audio.setAttribute('autoplay', 'autoplay');
+
+        $(document).keydown(function(e) {
+            kkeys.push(e.keyCode);
+            if(kkeys.toString().indexOf(seq) >= 0) {
+                audio.play();
+                kkeys = [];
+            }
+        })
+    });
 })(jQuery);
