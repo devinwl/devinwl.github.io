0a363c6a8fef34363ada3bae164151dda6183f99
Devin Lumley <devin@Devins-Air.lan>
2017-10-29 16:58:51 -0700

    Color scheme and content update for 2017

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
 (function($) {
-    $(document).ready(function() {
-
-        var kkeys = [];
-        var seq = '38,38,40,40,37,39,37,39,66,65';
-
-        var audio = document.createElement('audio');
-        audio.setAttribute('src', '../resources/sounds/secret.mp3');
-        //audio.setAttribute('autoplay', 'autoplay');
-
-        $(document).keydown(function(e) {
-            kkeys.push(e.keyCode);
-            if(kkeys.toString().indexOf(seq) >= 0) {
-                audio.play();
-                $('.alert').css('opacity', '1.0');
-                $('.alert').css('font-size', '36px');
-                $(audio).bind('ended', function() {
-                    $('.alert').css('opacity', '0.0');
-                    $('.alert').css('font-size', '0px');
-                });
-                kkeys = [];
-            }
-        })
-    });
+    //
 })(jQuery);
